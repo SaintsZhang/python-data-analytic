6f35b29498df16e561db0a315d102043e8c73bfe
SZHANG3 <SZHANG3@windows10.microdone.cn>
2018-04-20 12:49:08 +0800

    2nd chg

--- py-git-sz/com/stat/sz/ex_pd_001.py
+++ py-git-sz/com/stat/sz/ex_pd_001.py
@@ -13,10 +13,18 @@ data = pd.read_csv('airfares.txt', sep = '\s+',
                           'fare_2001', 'nb_passengers_2001']
                    )
 
-n_data = pd.DataFrame(data.iloc[:,0:7],
+data_2000 = pd.DataFrame(data.iloc[:,0:7],
                       columns = ['city1', 'city2', 'pop1', 'pop2',
                                  'dist', 'fare', 'nb_passengers', 'year'])
-n_data.iloc[:,7]=2000
-n_data.iloc[:,6]=data.iloc[:,6]
-n_data.iloc[:,5]=data.iloc[:,5]
-print(n_data.head(10))
+data_2000.iloc[:,7]=2000
+data_2000.iloc[:,6]=data.iloc[:,6]
+data_2000.iloc[:,5]=data.iloc[:,5]
+
+data_2001 = pd.DataFrame(data.iloc[:,[0,1,2,3,4,7,8]],
+                      columns = ['city1', 'city2', 'pop1', 'pop2',
+                                 'dist', 'fare', 'nb_passengers', 'year'])
+
+data_2001.iloc[:,7]=2001
+data_2001.iloc[:,6]=data.iloc[:,6]
+data_2001.iloc[:,5]=data.iloc[:,5]
+
